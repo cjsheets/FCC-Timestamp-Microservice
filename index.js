e0b5c85ec387f98c1f46a6ec7b9696828cb653c2
Chad Sheets <cjsheets@gmail.com>
2016-11-05 20:36:52 +0000

    Added timestamp detection

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 var http = require("http");
+const strftime = require('strftime')  
+const url = require('url');
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200, {
-    "Content-Type": "text/plain"
-  });
-  var body = '';
-  res.on("data", function(data){
-    body += data;
-  });
-  res.end(function(){
-    var date = new Date
-      return date.day();
-  });
-}).listen(80, "localhost");
+http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  var objUrl = url.parse(req.url, true),
+    re1 = /^\//, re2 = /%20/g, re3 = /^[0-9]+$/;
+  try {
+    var dateString = objUrl.pathname.replace(re1,'');
+    dateString = re3.test(dateString) ? +dateString : dateString.replace(re2,' ');
+    var date = new Date(dateString);
+    if(dateString && date != 'Invalid Date'){
+      res.end("{ unix:" + date.getTime() + ", natural: " + strftime('%B %d, %Y', date) + "}");
+    }
+    throw 'Invalid Date';
+  } catch(e) {
+    res.end("{ unix: null, natural: null}");
+  }
+}).listen(process.env.PORT, process.env.IP);
